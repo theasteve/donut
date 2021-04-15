b3379e3eabeccf8d2789cc90a36052c494949242
theasteve <stevenaguilarcs@gmail.com>
2021-04-15 18:50:33 -0400

    Create config.ru to start server using Rack
    Using Rack would make it easier to debug. This middleware would make it
    possible to intercept the requests and hit the breakpoints of
    binding.pry. To start the server run
    ```
    rackup config.ru -p 4567
    ```
    Related: https://github.com/rack/rack

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader' if settings.development?
+require 'dotenv/load' if settings.development?
+require 'slack'
+require 'pry'
+require './controllers/tasks_controller'
+
+class App < Sinatra::Base
+  use TasksController
+
+  Slack.configure do |config|
+    config.token = ENV['SLACK_BOT_TOKEN']
+  end
+
+  post '/interactions' do
+    payload = JSON.parse(params[:payload], symbolize_names: true)
+
+    client = Slack::Web::Client.new
+    user_id = payload[:user][:id]
+    channel_id = client.conversations_open(users: user_id).channel.id
+    client.chat_postMessage(channel: channel_id, text: "Hello, <@#{user_id}>!")
+
+    200
+  end
+
+  # # Use this to verify that your server is running and handling requests.
+  get '/' do
+    'Hello, world!'
+  end
+end
